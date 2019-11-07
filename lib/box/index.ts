7cccc3062bba51b014b81d65e29c7dec2a4fe219
Dan Shields <d.shieldsuk@gmail.com>
2019-11-07 21:37:22 +0000

    fix: stdin-write filename typo

--- lib/box/index.ts
+++ lib/box/index.ts
@@ -1,5 +1,5 @@
 import { spawn, ChildProcess } from 'child_process';
-import { writeToStdin } from '../sdtin-write';
+import { writeToStdin } from '../stdin-write';
 import { streamDataToString } from '../stream-to-string';
 import { Result } from '../types';
 
