e3acfbc4b149f0bec33b38ccd7fa95c29a2d1846
Dan Shields <d.shieldsuk@gmail.com>
2019-11-07 21:09:56 +0000

    feat: export compilation-only c functions

--- lib/c/index.ts
+++ lib/c/index.ts
@@ -1,9 +1,13 @@
 import { runCFile } from './run-file';
 import { runCSource } from './run-source';
+import { compileC } from './compile-file';
+import { compileCSource } from './compile-source';
 
 const c = {
     runFile: runCFile,
-    runSource: runCSource
+    runSource: runCSource,
+    compileFile: compileC,
+    compileSource: compileCSource
 };
 
 export default c;
